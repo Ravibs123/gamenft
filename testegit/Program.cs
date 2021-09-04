c485b506b199be8609bda2d0c89859a454f1bbe7
Felipe Wisniewski <felihpe55@gmail.com>
2021-09-03 23:05:16 -0300

    Projeto iniciado mostrando olá mundo

---
+++ testegit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace testegit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
